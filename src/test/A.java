3dc607456b27e038df61a59e3e2b5ce7f0540cef
ThulasiV <thulasi150595@gmail.com>
2025-05-16 15:54:14 +0530

    1st code push

---
+++ src/test/A.java
@@ -0,0 +1,30 @@
+package test;
+
+public class A extends B{
+	public A() {
+	  //  this("JAVA");
+	  super(12);
+	    System.out.println("Default const...");
+	  }
+
+	  //public A(int id) {
+	    //this(3456.5678f);
+	    //System.out.println(id);
+	  //}
+
+	//  public A(String name) {
+	   // this(12);
+	  // System.out.println(name);
+	  //}
+
+	  //public A(float sal) {
+	   // System.out.println(sal);
+	  //}
+
+	  public static void main(String[] args) {
+	    A a = new A();
+	  }
+
+	}
+
+
---
+++ src/test/B.java
@@ -0,0 +1,13 @@
+package test;
+
+public class B {
+	//public B() {
+	  //  System.out.println("Super class");
+	  //}
+	public B(int id) {
+	    System.out.println(id);
+	  }
+
+	}
+
+
